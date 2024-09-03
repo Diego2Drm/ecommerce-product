5121e6c8258a82f4d78eed1de9f3456b38272ed2
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-09-02 22:14:54 -0600

    Add product to cart

--- src/Components/ModalCart/ModalCart.jsx
+++ src/Components/ModalCart/ModalCart.jsx
@@ -1,16 +1,55 @@
-import React from "react";
-import style from "./ModalCart.module.css"
+import React, { useContext } from "react";
+import style from "./ModalCart.module.css";
+import { ShoppingShoesContext } from "../Context/Context";
+import iconDelete from "../../assets/Icons/icon-delete.svg";
 
-function ModalCart({openModal}){
-    return(
-        <section className={ openModal ? style.containerCart : style.close}>
-            <h3 className={style.title}>Cart</h3>
-            <span className={style.line}></span>
-            <p className={style.cartEmpty}>
-              Your cart is empty.
-            </p>
-        </section>
+function ModalCart({ openModal }) {
+  const context = useContext(ShoppingShoesContext);
+
+  const deleteProduct = () => {
+    context.setShoesCount((context.shoesCount = 0));
+  };
+
+  if (context.shoesCount == 0) {
+    return (
+      <section className={openModal ? style.containerCart : style.close}>
+        <h3 className={style.title}>Cart</h3>
+        <span className={style.line}></span>
+        <p className={style.cartEmpty}>Your cart is empty.</p>
+      </section>
     );
-};
+  } else {
+    return (
+      <section className={openModal ? style.containerCart : style.close}>
+        <h3 className={style.title}>Cart</h3>
+        <span className={style.line}></span>
+        <article className={style.article}>
+          <img
+            className={style.product}
+            src="https://i.ibb.co/jbnsxFZ/image-product-1-thumbnail.jpg"
+            alt=""
+          />
+          <div>
+            <p>Fall Limited Edition Sneakers</p>
+            <p>
+              <span>$125.00 x</span>
+              <span>{context.shoesCount}</span>
+              <span>${context.shoesCount * 125}.00</span>
+            </p>
+          </div>
+          <div className={style.delete}>
+            <img
+              className={style.iconDelete}
+              src={iconDelete}
+              alt="Delete"
+              onClick={deleteProduct}
+            />
+          </div>
+        </article>
+        <button className={style.checkout}>Checkout</button>
+      </section>
+    );
+  }
+}
 
 export { ModalCart };
